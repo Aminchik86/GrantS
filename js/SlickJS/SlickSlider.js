7bba9ee525f825ac023b40f74248ae6d62a4b7c6
Amin <78924745+Aminchik86@users.noreply.github.com>
2023-11-10 00:46:10 +0300

    Add files via upload

---
+++ js/SlickJS/SlickSlider.js
@@ -0,0 +1,5 @@
+$('.single-item').slick({
+    prevArrow: '<img src="img/mainSlider/arrows/Group-1.png">',
+    nextArrow: '<img src="img/mainSlider/arrows/Group.png">',
+    dots:true
+});
---
+++ js/best.js
@@ -0,0 +1,5 @@
+let bestPrice = document.querySelector('#BestPrice');
+
+bestPrice.addEventListener('click', function() {
+    alert('Лучшие товары, показаны на слайдере')
+});
